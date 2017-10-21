f23d584a00d9d3335a93a509661fd23e0ae7746d
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2017-10-22 01:40:36 +0300

    Find min and max value
    Lesson 2 homework

---
+++ MinAndMaxValue/MinAndMaxValue/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MinAndMaxValue
+{
+    class Program
+    {
+        static int MinValue(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                return b > c ? c : b;
+            }
+            else
+            {
+                return a > c ? c : a;
+            }    
+        }
+
+        static int MaxValue(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 3 values:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Min value: {MinValue(a, b, c)}");
+            Console.WriteLine("Enter 2 values:");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Max value: {MaxValue(a, b)}");
+            Console.ReadKey();
+        }
+    }
+}
