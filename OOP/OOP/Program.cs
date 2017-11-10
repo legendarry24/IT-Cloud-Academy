a152e190727eaba96ec24f101f9dad5dcf580951
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2017-11-10 19:57:36 +0200

    Lesson 8

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class MyClass
+    {
+        public MyClass()
+        {
+            Console.WriteLine("Without parameters");
+        }
+        public MyClass(int x): this()
+        {
+            Console.WriteLine("x");
+        }
+        public MyClass(int x, int y): this(x * y)
+        {
+            Console.WriteLine("x and y");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass test = new MyClass(2, 3);
+            
+            Console.ReadKey();
+        }
+    }
+}
