9fc5ace848574bcbde36ee30d2fda15f9bd05d51
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2017-11-13 21:29:32 +0200

    Lesson 9

---
+++ OOP2/OOP2/Counter.cs
@@ -0,0 +1,30 @@
+﻿using ClassLibrary1;
+
+namespace OOP2
+{
+    public class Counter: PublicTestClass
+    {
+        protected int Count;
+
+        public virtual void Increment()
+        {
+            a = Count--; // a from PublicTestClass
+            Count++;
+        }
+
+        public void TestMethod()
+        {
+            System.Console.WriteLine("Counter.TestMethod");
+        }
+
+        public int GetCount()
+        {
+            return Count;
+        }
+
+        public void Reset()
+        {
+            Count = 0;
+        }
+    }
+}
---
+++ OOP2/OOP2/MemoryCounter.cs
@@ -0,0 +1,20 @@
+﻿namespace OOP2
+{
+    class MemoryCounter: Counter
+    {
+        public void IncrementMemoryCount(int a)
+        {
+            Count = GetCount() + a;
+        }
+
+        public override void Increment() 
+        {
+            System.Console.WriteLine("MemoryCounter.Increment");
+        }
+
+        public new void TestMethod() // hide implementation (you should add the new key word)
+        {
+            System.Console.WriteLine("MemoryCounter.TestMethod");
+        }
+    }
+}
