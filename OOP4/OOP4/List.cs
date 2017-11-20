cfc20c2abeed482ccf187665ed28a487b0c60c53
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2017-11-20 21:37:08 +0200

    Lesson 12

---
+++ OOP4/OOP4/List.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4
+{
+    class List
+    {
+        private ListNode _head; // first element    
+        private ListNode _last;    // last element
+
+        public List(int value)
+        {
+            _head = new ListNode
+            {
+                Value = value
+            };
+            _last = _head;
+            ListNode.Index = 0;
+        }
+
+        public ListNode this[int index]
+        {
+            get
+            {
+                if (index < 0 && index >= ListNode.Index)
+                {
+                    return null;
+                }
+                while (_head.Next != _last.Next)
+                {
+                    if (ListNode.Index == index)
+                    {
+                        return _head;
+                    }
+                    _head = _head.Next;
+                }
+                return null;
+            }
+        }
+
+        public void Add(int value)
+        {
+            var node = new ListNode
+            {
+                Value = value
+            };
+
+            _last.Next = node;
+            _last = node;
+            ListNode.Index++;
+        }
+
+        public void Print()
+        {
+            while (_head.Next != _last.Next)
+            {
+                Console.WriteLine(_head.Value);
+                _head = _head.Next;
+            }
+            Console.WriteLine(_last.Value);
+        }
+
+    }
+}
---
+++ OOP4/OOP4/ListNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4
+{
+    class ListNode
+    {
+        public int Value { get; set; }
+
+        public ListNode Next { get; set; }
+
+        public static int Index { get; set; }
+    }
+}
