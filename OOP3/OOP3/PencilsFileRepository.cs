2a67daf07e914c010f75497504a22f8da1b4b658
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2017-11-20 18:23:34 +0200

    Lesson 10 + 11

---
+++ OOP3/OOP3/PencilsFileRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    class PencilsFileRepository : IPencilsRepository
+    {
+        public Pencil this[int index]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(Pencil pencil)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pencil Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
