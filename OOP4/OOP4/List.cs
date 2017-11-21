14b26be2d2e5dc955e5873d825e40d20e65af0d4
Igor Olkhovskiy <legendarry1337@gmail.com>
2017-11-22 00:08:21 +0200

    Update List.cs

--- OOP4/OOP4/List.cs
+++ OOP4/OOP4/List.cs
@@ -8,7 +8,7 @@ namespace OOP4
 {
     class List
     {
-        private ListNode _head; // first element    
+        private ListNode _head;    // first element    
         private ListNode _last;    // last element
 
         public List(int value)
@@ -18,29 +18,8 @@ namespace OOP4
                 Value = value
             };
             _last = _head;
-            ListNode.Index = 0;
         }
-
-        public ListNode this[int index]
-        {
-            get
-            {
-                if (index < 0 && index >= ListNode.Index)
-                {
-                    return null;
-                }
-                while (_head.Next != _last.Next)
-                {
-                    if (ListNode.Index == index)
-                    {
-                        return _head;
-                    }
-                    _head = _head.Next;
-                }
-                return null;
-            }
-        }
-
+       
         public void Add(int value)
         {
             var node = new ListNode
