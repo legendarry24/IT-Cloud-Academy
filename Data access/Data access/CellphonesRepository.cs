a180934b6a6b92f0cb9ee78adf0c6f289a4e123b
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2017-12-11 21:30:03 +0200

    Lesson 21

---
+++ Data access/Data access/CellphonesRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_access
+{
+    class CellphonesRepository : ICellphonesRepository
+    {
+        private const string RepositoryFilePath = @"S:\C-17-03\msr^_^\IT Cloud Academy\IT-Cloud-Academy\Data access\CellphonesData\cellphones.txt";
+
+        public void Add(Cellphone phone)
+        {
+            //Представляет обьект в виде строки
+            string phoneString = JsonConvert.SerializeObject(phone);
+            //File.AppendAllLines(RepositoryFilePath, new[] { phoneString });
+            //new implementation
+            using (StreamWriter streamWriter = new StreamWriter(RepositoryFilePath, true))
+                streamWriter.WriteLine(phoneString);
+        }
+
+        public IEnumerable<Cellphone> GetAll()
+        {
+            string[] phones = File.ReadAllLines(RepositoryFilePath);
+            //var cellphones = JsonConvert.DeserializeObject<Cellphone>(phoneString);
+            //return new[] { cellphones };
+            //new implementation
+            return phones.Select(x => JsonConvert.DeserializeObject<Cellphone>(x));
+        }
+
+        public void Remove(int id)
+        {
+            var phones = GetAll().Where(x => x.Id != id);
+            File.Open(RepositoryFilePath, FileMode.Truncate)
+                .Close();
+
+            foreach (var phone in phones)
+            {
+                Add(phone);
+            }
+        }
+    }
+}
--- Data access/Data access/Program.cs
+++ Data access/Data access/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,63 @@ namespace Data_access
     {
         static void Main(string[] args)
         {
+            //TextReader consoleStream = Console.In;
+            //TextWriter consoleOut = Console.Out;
+            //TextWriter consoleError = Console.Error;
+
+            //string filePath = @"S:\C-17-03\msr^_^\IT Cloud Academy\IT-Cloud-Academy\Data access\file.txt";
+            //string text = "Hello World!";
+            //File.WriteAllLines(filePath, new[] { text });
+            //var fileText = File.ReadAllText(filePath);
+            //Console.WriteLine(fileText);
+            #region using
+            //using (FileStream fileStream = File.Create(@"S:\C-17-03\msr^_^\IT Cloud Academy\IT-Cloud-Academy\Data access\file.txt"))
+            //{
+
+            //}
+
+            //the same 
+            //FileStream fileStream;
+
+            //try
+            //{
+            //    fileStream = File.Create(@"S:\C-17-03\msr^_^\IT Cloud Academy\IT-Cloud-Academy\Data access\file.txt");
+            //}
+            //finally
+            //{
+            //    fileStream.Dispose(); // Dispose invokes Close
+            //}
+            #endregion
+            //FileAttributes attributes = File.GetAttributes(filePath);
+            //Console.WriteLine(attributes);
+
+            var repository = new CellphonesRepository();
+
+            var cellphone = new Cellphone
+            {
+                Manufacturer = "Apple",
+                Model = "X",
+                Price = 1456.98m
+            };
+
+            var cellphone1 = new Cellphone
+            {
+                Manufacturer = "Samsung",
+                Model = "Galaxy S8",
+                Price = 1356.99m
+            };
+
+            repository.Add(cellphone);
+            repository.Add(cellphone1);
+            repository.Remove(0);
+            var phones = repository.GetAll();
+
+            foreach (var phone in phones)
+            {
+                Console.WriteLine(phone);
+            }
+
+            Console.ReadKey();
         }
     }
 }
