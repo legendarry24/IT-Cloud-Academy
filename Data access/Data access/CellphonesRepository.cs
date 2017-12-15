de043f93bcf3023f9d1713b6ec0c2c85e6dce292
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2017-12-15 03:39:02 +0200

    Lesson 21

---
+++ Data access/Data access/CellphonesRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_access
+{
+    class CellphonesRepository : ICellphonesRepository
+    {
+        private const string RepositoryFilePath = @"D:\Downloads\C#\IT Cloud Academy\IT-Cloud-Academy-master\Data access\CellphonesData\cellphones.txt";
+
+        public void Add(Cellphone phone)
+        {
+            //Represents an object as a string 
+            string phoneString = JsonConvert.SerializeObject(phone);
+            //File.AppendAllLines(RepositoryFilePath, new [] { phoneString} );
+            
+            //new implementation
+            // false by default, true => append, false => rewrite and save last line
+            using (StreamWriter streamWriter = new StreamWriter(RepositoryFilePath, true))
+                streamWriter.WriteLine(phoneString);
+        }
+
+        public IEnumerable<Cellphone> GetAll()
+        {
+            //for 1 object
+            //string phoneString = File.ReadAllText(RepositoryFilePath);
+            //var cellphone = JsonConvert.DeserializeObject<Cellphone>(phoneString);
+            //return new[] { cellphone };
+             
+            //new implementation
+            string[] phones = File.ReadAllLines(RepositoryFilePath);
+            return phones.Select(JsonConvert.DeserializeObject<Cellphone>);
+            //the same
+            //return phones.Select(x => JsonConvert.DeserializeObject<Cellphone>(x));
+        }
+
+        public void Remove(int id)
+        {
+            var phones = GetAll().Where(x => x.Id != id);
+            //clear file
+            File.Open(RepositoryFilePath, FileMode.Truncate)
+                .Close();
+            //and rewrite
+            foreach (var phone in phones)
+                Add(phone);
+        }
+
+        public void Print(IEnumerable<Cellphone> phones)
+        {
+            foreach (var phone in phones)
+                Console.WriteLine(phone);
+        }
+    }
+}
---
+++ Data access/Data access/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_access
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TextReader consoleStream = Console.In;
+            //TextWriter consoleOut = Console.Out;
+            //TextWriter consoleError = Console.Error;
+
+            //string filePath = @"S:\C-17-03\msr^_^\IT Cloud Academy\IT-Cloud-Academy\Data access\file.txt";
+            //string text = "Hello World!";
+            //File.WriteAllLines(filePath, new[] { text });
+            //var fileText = File.ReadAllText(filePath);
+            //Console.WriteLine(fileText);
+            #region using
+            //using (FileStream fileStream = File.Create(@"S:\C-17-03\msr^_^\IT Cloud Academy\IT-Cloud-Academy\Data access\file.txt"))
+            //{
+
+            //}
+
+            //the same 
+            //FileStream fileStream;
+
+            //try
+            //{
+            //    fileStream = File.Create(@"S:\C-17-03\msr^_^\IT Cloud Academy\IT-Cloud-Academy\Data access\file.txt");
+            //}
+            //finally
+            //{
+            //    fileStream.Dispose(); // Dispose invokes Close
+            //}
+            #endregion
+            //FileAttributes attributes = File.GetAttributes(filePath);
+            //Console.WriteLine(attributes);
+
+            var repository = new CellphonesRepository();
+
+            var cellphone = new Cellphone
+            {
+                Id = 0,
+                Manufacturer = "Apple",
+                Model = "X",
+                Price = 1456.98m
+            };
+            repository.Add(cellphone);
+
+            var cellphone1 = new Cellphone
+            {
+                Id = 1,
+                Manufacturer = "Samsung",
+                Model = "Galaxy S8",
+                Price = 1356.99m
+            };
+            repository.Add(cellphone1);
+
+            var phones = repository.GetAll();
+            repository.Print(phones);
+
+            repository.Remove(0);
+            phones = repository.GetAll();
+            Console.WriteLine("\nAfter remove id = 0:");
+            repository.Print(phones);
+
+            Console.ReadKey();
+        }
+    }
+}
