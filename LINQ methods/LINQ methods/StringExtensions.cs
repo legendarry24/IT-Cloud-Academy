5e5306769c83655cbfb86e85f2b6b88e6e0e3279
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2018-01-17 19:54:12 +0200

    LINQ

---
+++ LINQ methods/LINQ methods/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_methods
+{
+    static class StringExtensions
+    {
+        public static bool IsLengthEven(this string str)
+        {
+            return str.Length % 2 == 0;
+        }
+    }
+}
