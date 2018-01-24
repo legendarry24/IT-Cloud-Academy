70401d310c4f5bf4389bbe9bca88c222d8e83ab2
c <c>
2018-01-24 19:11:37 +0200

    Initial commit

--- ReflectionExample/ReflectionExample/Properties/AssemblyInfo.cs
+++ ReflectionExample/ReflectionExample/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("ReflectionExample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("ReflectionExample")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f85d85e4-ab53-4cfc-a11c-45d3bbb6166c")]
+[assembly: Guid("539646da-3e9c-48ba-aeb6-fb3e19a9fda9")]
 
 // Version information for an assembly consists of the following four values:
 //
