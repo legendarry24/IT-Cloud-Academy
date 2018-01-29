3e6cc161c463226999c14b17d905ca707f0a98e4
Igor Olkhovskiy <c>
2018-01-29 21:29:32 +0200

    MultiThreading

---
+++ MultiThreading/MultiThreading/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(TestMethod);
+            t.IsBackground = true;
+            t.Start();
+            Thread.Sleep(50);
+            t.Join();
+
+            Thread t1 = new Thread(TestMethod1);
+            Thread t2 = new Thread(TestMethod2);
+            t1.Priority = ThreadPriority.Lowest;
+            t2.Priority = ThreadPriority.Highest;
+            t1.Start(100);
+            t2.Start(50);
+            t2.Join();
+
+
+            Console.WriteLine("============FINISH!!!=============");
+        }
+
+        static void TestMethod()
+        {
+            Console.WriteLine("Start new Thread!");
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine("\nFinish Thread");
+        }
+
+        static void TestMethod1(object start)
+        {
+            int x = (int)start;
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write(1);
+            }
+            Console.WriteLine();
+        }
+
+        static void TestMethod2(object start)
+        {
+            int x = (int)start;
+            for (int i = 0; i < x; i++)
+            {
+                Console.Write(2);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ MultiThreading/MultiThreading2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MultiThreading2
+{
+    class Program
+    {
+        static double[] result = new double[1000000];
+        static Thread[] arrayThread;
+
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            Stopwatch st = new Stopwatch();
+            st.Start();
+
+            //Thread t1 = new Thread(Fill);
+            //Thread t2 = new Thread(Fill);
+            //t1.Start(0);
+            //t2.Start(1);
+
+            //t1.Join();
+            //t2.Join();
+                        
+            arrayThread = new Thread[count];
+
+            for (int i = 0; i < arrayThread.Length; i++)
+            {
+                arrayThread[i] = new Thread(Fill);
+                arrayThread[i].Start(i);
+                arrayThread[i].Join();
+            }
+
+            st.Stop();
+            Console.WriteLine(new string('+', 20));
+            Console.WriteLine($"Elapsed time: {st.ElapsedMilliseconds} mc");
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+
+        static void Fill(object x)
+        {
+            int part = (int)x;
+            // if length == 100
+            // x = 0 => part = 0 or x = 1 => part = 50
+            //int startIndex = (result.Length / 2) * part;
+            //int endIndex = startIndex + result.Length / 2;
+            int startIndex = (result.Length / arrayThread.Length) * part;
+            int endIndex = (startIndex + result.Length) / arrayThread.Length;
+
+            Random rand = new Random();
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = rand.NextDouble();
+            }
+        }
+    }
+}
