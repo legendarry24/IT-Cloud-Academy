e7b2ffe383e37544c40b75da9b1e9ae229471f7a
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2018-02-05 16:42:58 +0200

    Task 1 finished, Task 2 needs to be done

---
+++ MultiThreading_Test/Garden/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Garden
+{
+    class Program
+    {
+        static readonly byte[,] gardenArea = new byte[5, 15];
+        const byte firstGardener = 1;
+        const byte secondGardener = 2;
+
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => Work(0, firstGardener),
+                () => Work2(gardenArea.GetLength(0), gardenArea.GetLength(1), secondGardener)
+            );
+
+            Print();
+
+            Console.ReadKey();
+        }
+
+        static void Work(int start, byte value)
+        {
+            for (int i = start; i < gardenArea.GetLength(0); i++)
+            {
+                for (int j = start; j < gardenArea.GetLength(1); j++)
+                {
+                    //if another gardener has already done this garden plot then skip
+                    if (gardenArea[i, j] == secondGardener)
+                        continue;
+                    gardenArea[i, j] = value;
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        static void Work2(int startRow, int startColumn, byte value)
+        {
+            for (int i = startColumn - 1; i >= 0; i--)
+            {
+                for (int j = startRow - 1; j >= 0; j--)
+                {
+                    //if another gardener has already done this garden plot then skip
+                    if (gardenArea[j, i] == firstGardener)
+                        continue;
+                    gardenArea[j, i] = value;
+                    Thread.Sleep(50);
+                }
+            }
+        }
+
+        static void Print()
+        {
+            for (int i = 0; i < gardenArea.GetLength(0); i++)
+            {
+                for (int j = 0; j < gardenArea.GetLength(1); j++)
+                {
+                    Console.Write(gardenArea[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- MultiThreading_Test/MultiThreading_Test/Program.cs
+++ MultiThreading_Test/MultiThreading_Test/Program.cs
@@ -15,33 +15,80 @@ namespace MultiThreading_Test
         {
             methods = new Action[] { Method1, Method2, Method3 };
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < methods.Length; i++)
             {
                 Console.WriteLine($"If you want to execute method {i + 1} then click {i + 1}");
             }
-            int choice = int.Parse(Console.ReadLine());
-            MethodCall(choice);
-            //if (Console.ReadKey().Key == ConsoleKey.S)
+            Console.WriteLine("If you want to Cancel the current method - click E");
+            Test();
+
+            //while (true)
             //{
-            //    Console.WriteLine("Stop!");
-            //    for (int i = 0; i < _arrayThread.Length; i++)
-            //    {
-            //        _arrayThread[i].Abort();
-            //    }
+            //    object choice = Console.ReadLine();
+            //    Test(choice);
             //}
+
             Console.ReadKey();
         }
 
-        static void MethodCall(int number)
+        static void Test()
         {
-            Thread thread = new Thread(methods[number - 1].Invoke);
-            thread.Start();
+            CancellationTokenSource src = new CancellationTokenSource();
+            //int number 
+            //if (int.TryParse(obj as string, out number))
+            //{
+            //    Task task = MethodCall(number - 1, src.Token);
+            //    await task;
+            //}
+            //else
+            //{
+            //    string key = (string)obj;
+            //    if (key == "e")
+            //    {
+            //        Console.WriteLine("Stop the current method ");
+            //        //src.Cancel();
+            //        Thread.CurrentThread.Abort();
+            //    }
+            //}
+            while (true)
+            {
+                Thread thread = null;
+                char input = char.Parse(Console.ReadLine());
+                //int number = int.Parse(Console.ReadLine());
+                if (char.IsDigit(input))
+                {
+                    int number = (int) char.GetNumericValue(input);
+                     Task.Run(() =>
+                    {
+                        MethodCall(number - 1, src.Token);
+                        thread = Thread.CurrentThread;
+                    }, src.Token);
+                }
+                if (input == 'e')
+                {
+                    Console.WriteLine($"Stop the current method {Task.CurrentId}");
+                    src.Cancel();
+                    //thread.Abort();
+                }
+            }
+
+
+
+
+            
+        }
+
+        static void MethodCall(int number, CancellationToken token)
+        {
+            Task.Run(methods[number], token);
+            //Thread thread = new Thread(methods[number].Invoke);
+            //thread.Start();
         }
 
         static void Method1()
         {
             Console.WriteLine("Begin 1");
-            Thread.Sleep(5000);
+            Thread.Sleep(15000);
             Console.WriteLine("End 1");
         }
 
