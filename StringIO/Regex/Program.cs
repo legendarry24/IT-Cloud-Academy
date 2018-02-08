624cf2cc49adf74119b29385ae13953d32a51e42
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2018-02-09 01:48:24 +0200

    Added new regex

--- StringIO/Regex/Program.cs
+++ StringIO/Regex/Program.cs
@@ -9,73 +9,78 @@ namespace RegExp
         {
             string pattern = @"^\+38\(0\d{2}\)\d{3}-?\d{2}-?\d{2}$|^\+380\d{9}$";
 
-            if (Regex.IsMatch(phoneNumber, pattern))
-            {
-                Console.WriteLine($"{phoneNumber} is valid ukrainian phone number");
-            } 
-            else
-            {
-                Console.WriteLine($"{phoneNumber} is invalid ukrainian phone number");
-            }
+            Console.WriteLine(Regex.IsMatch(phoneNumber, pattern)
+                ? $"{phoneNumber} is valid ukrainian phone number"
+                : $"{phoneNumber} is invalid ukrainian phone number");
         }
         
         public static bool Email(string s)
         {
             Regex regex = new Regex(@"^\w+([-.]\w+)*@\w+\.\w+([-.]\w+)*$");
-	    return regex.IsMatch(s)? true: false;
+
+            return regex.IsMatch(s);
+        }
+
+        static string RemoveSpecialCharacters(string input)
+        {
+            string pattern = @"[a-z]\*+[a-z]";
+
+            return Regex.Replace(input, pattern, string.Empty);
         }
 
         static void Main(string[] args)
         {
-            string[] values = { "gray", "lane" };
-            string pattern = @"[ae]";
+            //string[] values = { "gray", "lane" };
+            //string pattern = @"[ae]";
 
-            foreach (string value in values)
-            {
-                Console.WriteLine($"Input: {value}");
-                var matches = Regex.Matches(value, pattern);
-
-                foreach (Match match in matches)
-                    Console.WriteLine($"Match: {match.Value}");
-
-                Console.WriteLine();
-            }
-
-            pattern = @"[^rn]";
-
-            foreach (string value in values)
-            {
-                Console.WriteLine($"Input: {value}");
-                var matches = Regex.Matches(value, pattern);
-
-                foreach (Match match in matches)
-                    Console.WriteLine($"Match: {match.Value}");
-
-                Console.WriteLine();
-            }
-
-            string phoneNumber = "+380993426174";
-            string phoneNumber2 = "+38(099)3426174";
-            string phoneNumber3 = "0993426174";
-            string phoneNumber4 = "+38993426174";
-            string phoneNumber5 = "+38(099)342-61-71"; 
-
-            ValidPhoneNumber(phoneNumber);
-            ValidPhoneNumber(phoneNumber2);
-            ValidPhoneNumber(phoneNumber3);
-            ValidPhoneNumber(phoneNumber4);
-            ValidPhoneNumber(phoneNumber5);
-
-            //while (true)
+            //foreach (string value in values)
             //{
-            //    Console.Write("Input phone number: ");
-            //    ValidPhoneNumber(Console.ReadLine());
+            //    Console.WriteLine($"Input: {value}");
+            //    var matches = Regex.Matches(value, pattern);
+
+            //    foreach (Match match in matches)
+            //        Console.WriteLine($"Match: {match.Value}");
+
+            //    Console.WriteLine();
             //}
+
+            //pattern = @"[^rn]";
+
+            //foreach (string value in values)
+            //{
+            //    Console.WriteLine($"Input: {value}");
+            //    var matches = Regex.Matches(value, pattern);
+
+            //    foreach (Match match in matches)
+            //        Console.WriteLine($"Match: {match.Value}");
+
+            //    Console.WriteLine();
+            //}
+
+            //string phoneNumber = "+380993426174";
+            //string phoneNumber2 = "+38(099)3426174";
+            //string phoneNumber3 = "0993426174";
+            //string phoneNumber4 = "+38993426174";
+            //string phoneNumber5 = "+38(099)342-61-71"; 
+
+            //ValidPhoneNumber(phoneNumber);
+            //ValidPhoneNumber(phoneNumber2);
+            //ValidPhoneNumber(phoneNumber3);
+            //ValidPhoneNumber(phoneNumber4);
+            //ValidPhoneNumber(phoneNumber5);
+
+            ////while (true)
+            ////{
+            ////    Console.Write("Input phone number: ");
+            ////    ValidPhoneNumber(Console.ReadLine());
+            ////}
             
-            Console.WriteLine(Email("www.aa_bb.a@gmail.com.ua"));
-            Console.WriteLine(Email("www_a-a.a@gmail.com"));
-            Console.WriteLine(Email("www_a-a.agmail.com.ua"));
-            Console.WriteLine(Email("a-a.a@gmail"));
+            //Console.WriteLine(Email("www.aa_bb.a@gmail.com.ua"));
+            //Console.WriteLine(Email("www_a-a.a@gmail.com"));
+            //Console.WriteLine(Email("www_a-a.agmail.com.ua"));
+            //Console.WriteLine(Email("a-a.a@gmail"));
+
+            Console.WriteLine(RemoveSpecialCharacters("zxcv****bawp"));
 
             Console.ReadKey();
         }
