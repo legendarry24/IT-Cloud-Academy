9611052a18603074795de010d004653d8ade790c
Igor Olkhovskiy <c>
2018-02-14 21:33:47 +0200

    MVC lesson 2

---
+++ MVC App/MVC App/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_App.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var cookies = HttpContext.Request.Cookies;
+			return View();
+		}
+
+		public ActionResult TestAction1()
+		{
+			string name = "Alex";
+
+			ViewBag.Name = name;
+			ViewData["Name"] = name + " view data";
+			TempData["Name"] = name + " temp data";
+
+			Response.Write("Hey it's a response");
+			
+			return View("TestAction1");
+		}
+
+		public ActionResult RedirectToUrl()
+		{
+			return Redirect("http://google.com");
+		}
+
+		public ActionResult RedirectToContact()
+		{
+			HttpContext.Response.SetCookie(new HttpCookie("CookieName", "CookieValue"));
+			return RedirectToAction("Contact");
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
