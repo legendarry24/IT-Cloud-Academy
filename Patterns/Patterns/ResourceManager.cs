e5b51dbd491fa52a934413da56254b868090a4e5
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2021-08-28 02:21:37 +0300

    Refactoring of the Dispose pattern implementation
    - added useful comments
    - code style changes and improvements

--- Patterns/Patterns/ResourceManager.cs
+++ Patterns/Patterns/ResourceManager.cs
@@ -1,52 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Patterns
 {
 	class ResourceManager : IDisposable
 	{
 		private bool disposed = false;
-		private string _name { get; set; }
+
+		private string Name { get; }
 
 		public ResourceManager(string name)
 		{
-			_name = name;
+			this.Name = name;
 		}
 
 		public void Dispose()
 		{
-			Dispose(true);
-			Console.WriteLine(_name);
-			//to prevent destructor's invocation
-			//GC.SuppressFinalize(this);
+			Console.WriteLine(this.Name);
+
+			// true means that the Dispose method is called from the managed code and should clean up both managed and unmanaged resources
+			this.Dispose(true);
+			// prevent the Finalizer's invocation (remove pointer to this object from finalization queue)
+			// this will prevent the finalizer from releasing unmanaged resources that have already been freed
+			GC.SuppressFinalize(this);
 		}
 
-		private void Dispose(bool disposing)
+		// derived classes can override this method to implement their own clean up logic
+		protected virtual void Dispose(bool disposing)
 		{
-			if (disposed)
+			if (this.disposed)
+			{
 				return;
+			}
 
 			if (disposing)
 			{
-				Console.WriteLine("Clean managed resources");
-				Console.WriteLine(_name);
-				//clean any managed resources
+				// clean any managed resources
+				// for example, call Dispose on instances of managed wrappers of unmanaged resources (e.g. SqlConnection class)
+				Console.WriteLine("Free managed resources");
+				Console.WriteLine(this.Name);
 			}
 
-			//clean unmanaged resources
-			Console.WriteLine("Clean unmanaged resources");
-			Console.WriteLine(_name);
-			disposed = true;
+			// clean unmanaged resources
+			Console.WriteLine("Free unmanaged resources");
+			Console.WriteLine(this.Name);
+
+			// set flag to true to avoid errors while disposing the resources that have already been disposed
+			this.disposed = true;
 		}
 
+		// overriding of the Finalizer is required only in case when the class operates unmanaged resources directly
 		~ResourceManager()
 		{
-			Console.WriteLine("destructor");
-			Console.WriteLine(_name);
-			Dispose(false);
+			Console.WriteLine("Finalizer");
+			Console.WriteLine(this.Name);
+
+			// false means that the Dispose method is called from the unmanaged code and should not clean up managed resources.
+			// in this way only unmanaged resources will be disposed during finalization. Because you can get an error if you try to 
+			// dispose managed resources from finalizer as they could have already been collected by GC
+			this.Dispose(false);
 		}
 	}
 }
